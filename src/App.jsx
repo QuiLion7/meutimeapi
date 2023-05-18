fb15004e83857fe1faf45b636d9d3a3b471d6336
QuiLion7 <quilbrub@gmail.com>
2023-05-18 06:29:04 -0300

    Fetch de países OK

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>testando</h1>
+      <Home  />
+    </div>
+  )
+}
+
+export default App
